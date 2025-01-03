c6e512aa391122c055f20c3fb688e006eb0b8a35
David TA <td.ta.david@gmail.com>
2025-01-03 20:54:26 +0100

    chore: fix

---
+++ Apirest/APIRest-2D-interface-project/DataAccess/Migrations/20250103194638_Snapshotgeneration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIRest_2D_interface_project.Migrations
+{
+    /// <inheritdoc />
+    public partial class Snapshotgeneration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
