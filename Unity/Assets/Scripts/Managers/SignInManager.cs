3cc2adaefe3233b52d7f2670e624faa862f580fe
march67 <td.ta.david@gmail.com>
2025-01-17 16:54:28 +0100

    feature(Unity-UI): work in progress for SignInManager and about to create Unity Models to handle request and response to DTOs of the API Rest

--- Unity/Assets/Scripts/Managers/SignInManager.cs
+++ Unity/Assets/Scripts/Managers/SignInManager.cs
@@ -1,16 +1,37 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SignInManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TMP_InputField usernameInput;
+    [SerializeField] private TMP_InputField passwordInput;
+    [SerializeField] private TMP_InputField emailInput;
+    [SerializeField] private Button signInButton;
+    [SerializeField] private TextMeshProUGUI errorText;
+
     void Start()
+    {
+        signInButton.onClick.AddListener(HandleSignin);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleSignIn()
     {
-        
+        StartCoroutine(SignInCouroutine);
+    }
+
+    private IEnumerator SignInCoroutine()
+    {
+        var signInData = new SignInRequest
+        {
+
+        }
     }
 }
