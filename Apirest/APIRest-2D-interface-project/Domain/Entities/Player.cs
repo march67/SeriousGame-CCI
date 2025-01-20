c40dd3de4eccc655dabbb2d07f8e41fa2b0f800d
march67 <td.ta.david@gmail.com>
2025-01-20 16:45:14 +0100

    feature(apirest-player-currency): work in progress for PlayerController, entity done with database update

---
+++ Apirest/APIRest-2D-interface-project/Domain/Entities/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace APIRest_2D_interface_project.Domain.Entities
+{
+    public class Player
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+
+        public int Level { get; set; }
+
+        public int Gems { get; set; }
+
+        public int Gold { get; set; }
+    }
+}
---
+++ Apirest/APIRest-2D-interface-project/Presentation/Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using APIRest_2D_interface_project.Business.Services.Interfaces;
+using APIRest_2D_interface_project.DataAccess.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIRest_2D_interface_project.Presentation.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IUserService _userService;
+    }
+}
